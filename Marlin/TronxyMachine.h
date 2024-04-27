4d8156a561e4577c4bc6ca63356bbb771245bdae
Pedro Sanchez <pedrsanchez@gmail.com>
2024-04-26 21:22:28 -0400

    feat: tronxy xy-2 pro fix e-step reverse

--- Marlin/TronxyMachine.h
+++ Marlin/TronxyMachine.h
@@ -20,7 +20,7 @@
 #define UI_MARLIN_DEFAULT  0
 #define UI_PIC480X320      1
 
-#define TRONXY_PROJ        PROJ_XY3SE
+#define TRONXY_PROJ        PROJ_XY2_PRO
 #define TRONXY_UI          UI_PIC480X320 // UI_PIC480X320 or UI_MARLIN_DEFAULT
 
 #if TRONXY_UI > 0
@@ -33,17 +33,17 @@
 #if TRONXY_PROJ == PROJ_XY2_PRO
 
 #define TRONXY_MACHINE_NAME       "XY2-PRO"
-#define TRONXY_BACKUP_POWER       0 //UPS功能,注意需要某些特定脚作断电检测,配置相应的中断函数
-#define TRONXY_SCREEN_SHUTDOWN    1 //熄屏功能
-#define TRONXY_HOME_XY_ONLY       0 //手动调试界面复位是否只有XY复位,如果是,则Z复位按钮激活,否则失效
-#define WIFI_INTERFACE            0 //WIFI功能
+#define TRONXY_BACKUP_POWER       1 //Função UPS, observe que certos pinos são necessários para detecção de falha de energia e configure a função de interrupção correspondente.
+#define TRONXY_SCREEN_SHUTDOWN    1 //Função de desligamento de tela
+#define TRONXY_HOME_XY_ONLY       0 //A redefinição manual da interface de depuração é apenas redefinição XY? Nesse caso, o botão de redefinição Z é ativado, caso contrário, é inválido.
+#define WIFI_INTERFACE            1 //Função WIFI
 #define TAOLI_SERIAL              0
-#define LASER_SERIAL              0 //wifi端子用于激光PWM控制
-#define DUAL_ZMAX_HOME            0 //大端复位,向反方向复位,再回到正方向找0点
-#define Z_BED_MOVE                0 //Z轴移动:热床
-#define EXTRUDERS                 1 //挤出机数量
-#define INVERT_E0_DIR             true //TITAN:true general:false
-#define USE_EXTRUDER              1 //T,g TITAN:1,BMG:2,TAOLI:3,general:4
+#define LASER_SERIAL              0 //wifiTerminal para controle laser PWM
+#define DUAL_ZMAX_HOME            0 //Redefinição da extremidade grande, redefinição na direção oposta e, em seguida, retornar à direção direta para encontrar o ponto 0
+#define Z_BED_MOVE                0 //Z Movimento do eixo: cama aquecida
+#define EXTRUDERS                 1 //Número de extrusoras
+#define INVERT_E0_DIR             false //TITAN:true general:false
+#define USE_EXTRUDER              4 //T,g TITAN:1,BMG:2,TAOLI:3,general:4
 #define FIX_MOUNTED_PROBE
 
 #elif TRONXY_PROJ == PROJ_XY3_PRO
